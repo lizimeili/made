3a872a79e1e2045cb8b93c7e5d48c4467e79b547
lizimeili <122461767+lizimeili@users.noreply.github.com>
2023-02-04 17:04:19 +0800

    Add files via upload

---
+++ made(未完成).py
@@ -0,0 +1,38 @@
+from random import randint
+
+
+print("made")
+print("by:lizimeili")
+input()
+a = input()
+b = "1"
+c = "2"
+print("玩文字游戏输入1,随机数输入2")
+print(a)
+
+
+if a == b:
+    a = 1
+
+
+if a == c:
+    a = 2
+
+
+if a == 1:
+    print("嗨!")
+    print("好无聊")
+    print("你叫什么名字?")
+    input()
+    print("哦!")
+    print("知道了!")
+
+
+if a == 2:
+    a = randint(1, 100)
+    
+    
+    print(a)
+    input() 
+
+
